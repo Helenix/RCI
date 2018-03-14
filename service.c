c1bdc312c6f40ffca69ae0e0fcefe4bee099bea2
João Pedro Dimas Heleno <joaoheleno1995@gmail.com>
2018-03-14 13:17:28 +0000

    Add files via upload

--- service.c
+++ service.c
@@ -11,8 +11,10 @@
 
 int main (int argc, char * argv[]) {
     int i, centralServerLength, serviceID;
+    int centralServerSocket;
     unsigned centralServerPort = 59000, udpPort, tcpPort;
     char *centralServerIP = NULL, *serviceServerIP = NULL;
+    char message[128], buffer[128];
     struct sockaddr_in centralServer;
     struct hostent *host;
     bool isDefaultServer = true;
@@ -47,36 +49,61 @@ int main (int argc, char * argv[]) {
     }
 
     // UDP client for central server requests
-    if(isDefaultServer) {
-        printf("Default central Server\n");
+    centralServerSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(centralServerSocket == -1) {
+        exit(-1);
+    }
+
+    memset((void*)&centralServer,(int)'\0', sizeof(centralServer));
+    centralServer.sin_family = AF_INET;
+    if(isDefaultServer || centralServerIP == NULL) { 
         if((host = gethostbyname("tejo.tecnico.ulisboa.pt")) == NULL) {
             exit(-1);
         }
-
-        memset((void*)&centralServer,(int)'\0', sizeof(centralServer));
-		centralServer.sin_family = AF_INET;
 		centralServer.sin_addr.s_addr = ((struct in_addr *)(host->h_addr_list[0]))->s_addr;
-		centralServer.sin_port = htons((u_short)centralServerPort);
-        centralServerLength = sizeof(centralServerLength);
     } else {
-        printf("Custom central Server\n");
-
-        memset((void*)&centralServer,(int)'\0', sizeof(centralServer));
-		centralServer.sin_family = AF_INET;
-        if(centralServerIP == NULL) {
-            if((host = gethostbyname("tejo.tecnico.ulisboa.pt")) == NULL) {
-                exit(-1);
-            }
-            centralServer.sin_addr.s_addr = ((struct in_addr *)(host->h_addr_list[0]))->s_addr;   
-        } else {
-            inet_aton(centralServerIP, &centralServer.sin_addr);
-        }
-		centralServer.sin_port = htons((u_short)centralServerPort);
-        centralServerLength = sizeof(centralServer);
+        inet_aton(centralServerIP, &centralServer.sin_addr);
     }
+    centralServer.sin_port = htons((u_short)centralServerPort);
+    centralServerLength = sizeof(centralServer);
 
-    printf("Host ip  : %s \n", inet_ntoa(centralServer.sin_addr));
-    printf("Host port: %d \n", ntohs(centralServer.sin_port));
+    // Central server and service information
+    if(isDefaultServer) {
+        printf("Default central Server\n");
+    }
+    else {
+        printf("Custom central Server\n");
+    }
+    printf("Central Server IP  : %s \n", inet_ntoa(centralServer.sin_addr));
+    printf("Central Server port: %d \n", ntohs(centralServer.sin_port));
+    printf("Service IP  : %s \n", serviceServerIP);
+    printf("Service ID  : %d \n", serviceID);   
+    printf("Service udp port: %d \n", udpPort);
+    printf("Service tcp port: %d \n", tcpPort);
+    printf("\nType 'help' for valid commands\n");
+
+    while(1) {
+        fgets(buffer, sizeof(buffer), stdin);
+        sscanf(buffer,"%[^\n]s", message);
+
+        if(!strcmp("join x",message)) {
+
+        } else if(!strcmp("show_state",message)) {
+
+        } else if(!strcmp("leave",message)) {
+            
+        } else if(!strcmp("exit",message)) {
+            break;
+        } else if(!strcmp("help",message)) {
+          printf("Valid commands: \n");
+          printf("-> join x \n");
+          printf("-> show_state \n");
+          printf("-> leave \n");
+          printf("-> exit \n");  
+        } else {
+            printf("Invalid command! Type 'help'\n");
+        }
+    }
 
     if(centralServerIP != NULL) {
         free(centralServerIP);
